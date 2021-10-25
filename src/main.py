327cdf899c39463785b0414d305474dfba3ce6e1
Julien Masclef <julien.masclef@gmail.com>
2021-10-25 07:34:29 +0200

    Init

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello, World !</h1>"
+
 if __name__=='__main__':
-    print("Hello, World!")
+    app.run()
+
