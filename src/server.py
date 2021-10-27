b8a09bece0ca316120046bd8804f538533c42631
Julien Masclef <julien.masclef@gmail.com>
2021-10-27 11:11:04 +0200

    Docker parameters

--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
 import flask
+import sys
 from flask import Flask
 from flask import render_template
+
+sys.path.append(".")
 app=Flask(__name__)
 
 @app.route('/')
@@ -8,5 +11,5 @@ def index():
     return render_template('index.html',name='World')
 
 if __name__=='__main__':
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
 
